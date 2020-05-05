cf15858f051aa48873f9d0037f7c8bebf0fbcd79
aSlug <marpi93@icloud.com>
2020-05-05 15:28:37 +0200

    scaffold with mock card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import FlexView from "react-flexview";
+import * as _ from "lodash";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlexView className="App" hAlignContent="center">
+      <FlexView column>
+        <FlexView column className="group">
+          {_.chunk(cards, 5).map((row, i) => (
+            <FlexView className="row">
+              {row.map((card) => (
+                <img
+                  className="card"
+                  style={{ margin: "3px", width: "200px" }}
+                  alt={card.name}
+                  title={card.name}
+                  src="https://img.scryfall.com/cards/normal/front/7/f/7f3fff7e-f34d-4a99-a805-bd66c4e9f0cb.jpg?1562614406"
+                />
+              ))}
+            </FlexView>
+          ))}
+        </FlexView>
+      </FlexView>
+    </FlexView>
   );
 }
 
 export default App;
+
+const cards = [
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+  {
+    name: "Yasova Dragonclaw",
+    imgUrl:
+      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
+  },
+];
