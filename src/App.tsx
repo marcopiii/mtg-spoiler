8f1bc229c7492b95a24791ac0e28bb75ef73796d
aSlug <marpi93@icloud.com>
2020-05-08 18:22:48 +0200

    display cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,89 @@
 import React from "react";
 import "./App.css";
 import FlexView from "react-flexview";
-import * as _ from "lodash";
+import _ from "lodash";
+import * as Scry from "scryfall-sdk";
 
-function App() {
-  return (
-    <FlexView className="App" hAlignContent="center">
-      <FlexView column>
-        <FlexView column className="group">
-          {_.chunk(cards, 5).map((row, i) => (
-            <FlexView className="row">
-              {row.map((card) => (
-                <img
-                  className="card"
-                  style={{ margin: "3px", width: "200px" }}
-                  alt={card.name}
-                  title={card.name}
-                  src="https://img.scryfall.com/cards/normal/front/7/f/7f3fff7e-f34d-4a99-a805-bd66c4e9f0cb.jpg?1562614406"
-                />
+type Props = {
+  cardList: string[];
+};
+
+type State = {
+  collection: Scry.Card[];
+};
+
+export class App extends React.Component<Props, State> {
+  state = { collection: [] as Scry.Card[] };
+
+  componentDidMount() {
+    const identifiers = this.props.cardList.map((cardName) =>
+      Scry.CardIdentifier.byName(cardName)
+    );
+    Scry.Cards.collection(...identifiers)
+      .waitForAll()
+      .then((collection) => {
+        this.setState({ collection: collection });
+      });
+  }
+
+  render() {
+    const { collection } = this.state;
+    const groups = [
+      collection.filter(isMonoW).sort(byRarity),
+      collection.filter(isMonoU).sort(byRarity),
+      collection.filter(isMonoB).sort(byRarity),
+      collection.filter(isMonoR).sort(byRarity),
+      collection.filter(isMonoG).sort(byRarity),
+      collection.filter(isMulticolor).sort(byRarity),
+      collection.filter(isColorless).sort(byRarity),
+      collection.filter(isLand).sort(byRarity),
+    ];
+
+    return (
+      <FlexView className="App" hAlignContent="center">
+        <FlexView column>
+          {groups.map((group, i) => (
+            <FlexView column className="group" key={i}>
+              {_.chunk(group, 5).map((row, i) => (
+                <FlexView className="row" key={i}>
+                  {row.map((card, i) => (
+                    <img
+                      className="card"
+                      alt={card.name}
+                      title={card.name}
+                      src={card.image_uris ? card.image_uris.normal : undefined}
+                      key={i}
+                    />
+                  ))}
+                </FlexView>
               ))}
             </FlexView>
           ))}
         </FlexView>
       </FlexView>
-    </FlexView>
-  );
+    );
+  }
 }
 
 export default App;
 
-const cards = [
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-  {
-    name: "Yasova Dragonclaw",
-    imgUrl:
-      "https://img.scryfall.com/cards/large/front/4/9/490994c6-9fef-482b-835d-a64350bfaa8d.jpg?1562824820",
-  },
-];
+const isMonoW = (c: Scry.Card) =>
+  c.colors && c.colors.length === 1 && c.colors.includes("W");
+const isMonoU = (c: Scry.Card) =>
+  c.colors && c.colors.length === 1 && c.colors.includes("U");
+const isMonoB = (c: Scry.Card) =>
+  c.colors && c.colors.length === 1 && c.colors.includes("B");
+const isMonoR = (c: Scry.Card) =>
+  c.colors && c.colors.length === 1 && c.colors.includes("R");
+const isMonoG = (c: Scry.Card) =>
+  c.colors && c.colors.length === 1 && c.colors.includes("G");
+const isMulticolor = (c: Scry.Card) => c.colors && c.colors.length > 1;
+const isColorless = (c: Scry.Card) =>
+  c.colors && c.colors.length === 0 && !c.type_line.includes("Land");
+const isLand = (c: Scry.Card) => c.type_line.includes("Land");
+
+const byRarity = (a: Scry.Card, b: Scry.Card) => {
+  const rarityScore = (r: string) =>
+    r === "mythic" ? 0 : r === "rare" ? 1 : r === "uncommon" ? 2 : 3;
+  return rarityScore(a.rarity) - rarityScore(b.rarity);
+};
